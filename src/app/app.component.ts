0c332ceb6268e41e628db56739e42b81701343b4
Oybek Jumaniyozov <jumaniyozov@oybek.com>
2023-12-14 19:12:42 +0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,45 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    FormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
 export class AppComponent {
-  title = 'item-project';
+  public title: string = 'item-project';
+  public name: string = "";
+  public counter: number = 0;
+
+  public newName: string = "";
+  public names: string[] = [];
+
+  public up(): void {
+    this.counter++;
+  }
+
+  public down(): void {
+    this.counter--;
+  }
+
+  public addNewName(): void {
+    if (this.newName) {
+      this.names.push(this.newName);
+      this.newName = "";
+    }
+  }
+
+  public removeNameAt(index: number): void {
+    if (this.names.length > index) {
+      this.names.splice(index, 1);
+    }
+  }
 }
